cf97a2a58843f35bb335c7b9d1cecbd0c3bd6854
nandni <nandnikorat4305@gmail.com>
2026-01-07 10:32:49 +0530

    first commit

--- app/src/main/java/com/example/instrumentalapp/MainActivity.java
+++ app/src/main/java/com/example/instrumentalapp/MainActivity.java
@@ -17,45 +17,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Link UI cards
-        btnGuitar = findViewById(R.id.btnGuitar);
-
-
-        // Guitar click
-        btnGuitar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                animateClick(v, new Runnable() {
-                    @Override
-                    public void run() {
-                        startActivity(new Intent(MainActivity.this, GuitarActivity.class));
-                    }
-                });
-            }
-        });
-
-
-
-    // 🎯 MICRO-INTERACTION ANIMATION (Designer Touch)
-    private void animateClick(final View view, final Runnable action) {
-
-        // Scale down
-        view.animate()
-                .scaleX(0.96f)
-                .scaleY(0.96f)
-                .setDuration(80)
-                .withEndAction(new Runnable() {
-                    @Override
-                    public void run() {
-
-                        // Scale back up
-                        view.animate()
-                                .scaleX(1f)
-                                .scaleY(1f)
-                                .setDuration(80)
-                                .withEndAction(action)
-                                .start();
-                    }
-                })
-                .start();
     }
 }

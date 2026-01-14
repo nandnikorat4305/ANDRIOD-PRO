d7f46e1cfa235922ddcbba04e2e5f97e07548ef4
nandnikorat <nandnikorat4305@gmail.com>
2026-01-14 11:43:01 +0530

    GuitarActivity.java sounds adding

--- app/src/main/java/com/example/instrumentalapp/GuitarActivity.java
+++ app/src/main/java/com/example/instrumentalapp/GuitarActivity.java
@@ -11,3 +11,14 @@ public class GuitarActivity extends AppCompatActivity {
 
     ImageView s1, s2, s3, s4, s5, s6;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guitar);
+
+        s1 = findViewById(R.id.string1);
+        s2 = findViewById(R.id.string2);
+        s3 = findViewById(R.id.string3);
+        s4 = findViewById(R.id.string4);
+        s5 = findViewById(R.id.string5);
+        s6 = findViewById(R.id.string6);

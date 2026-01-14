11f185436c34776fa2ebd5620b7211eee9b91d9d
nandnikorat <nandnikorat4305@gmail.com>
2026-01-14 11:41:03 +0530

    GuitarActivity.java starting

---
+++ app/src/main/java/com/example/instrumentalapp/GuitarActivity.java
@@ -0,0 +1,13 @@
+package com.example.instrumentalapp;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GuitarActivity extends AppCompatActivity {
+
+    ImageView s1, s2, s3, s4, s5, s6;
+

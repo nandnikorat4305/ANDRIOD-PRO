83621a626ecf5a3abbf4dd304c6ecb753b7b3172
nandnikorat <nandnikorat4305@gmail.com>
2026-01-14 11:57:44 +0530

    GuitarActivity.java adding sounds 2

--- app/src/main/java/com/example/instrumentalapp/GuitarActivity.java
+++ app/src/main/java/com/example/instrumentalapp/GuitarActivity.java
@@ -7,6 +7,8 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.instrumentalapp.R;
+
 public class GuitarActivity extends AppCompatActivity {
 
     ImageView s1, s2, s3, s4, s5, s6;
@@ -22,3 +24,21 @@ public class GuitarActivity extends AppCompatActivity {
         s4 = findViewById(R.id.string4);
         s5 = findViewById(R.id.string5);
         s6 = findViewById(R.id.string6);
+
+        s1.setOnTouchListener((v, e) -> play(R.raw.string1, e));
+        s2.setOnTouchListener((v, e) -> play(R.raw.string2, e));
+        s3.setOnTouchListener((v, e) -> play(R.raw.string3, e));
+        s4.setOnTouchListener((v, e) -> play(R.raw.string4, e));
+        s5.setOnTouchListener((v, e) -> play(R.raw.string5, e));
+        s6.setOnTouchListener((v, e) -> play(R.raw.string6, e));
+    }
+
+    private boolean play(int sound, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            MediaPlayer mp = MediaPlayer.create(this, sound);
+            mp.setOnCompletionListener(MediaPlayer::release);
+            mp.start();
+        }
+        return true;
+    }
+}
